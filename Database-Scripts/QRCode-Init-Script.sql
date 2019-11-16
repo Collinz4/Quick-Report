0832bd653657a0c9784340d1d04ca4cc3ce30f28
Stephen Collins <stephen436901@gmail.com>
2019-11-16 16:34:17 -0500

    Added SQL Initialization script.

---
+++ Database-Scripts/QRCode-Init-Script.sql
@@ -0,0 +1,30 @@
+
+
+DROP SCHEMA IF EXISTS qr_report_v1;
+CREATE SCHEMA IF NOT EXISTS qr_report_v1 DEFAULT CHARACTER SET utf8 ;
+USE qr_report_v1 ;
+
+
+DROP TABLE IF EXISTS qr_report_v1.Users;
+CREATE TABLE IF NOT EXISTS qr_report_v1.Users (
+    id BINARY(16) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    passwordHash VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY(id)
+) ENGINE = InnoDB;
+
+
+DROP TABLE IF EXISTS qr_report_v1.Assets;
+CREATE TABLE IF NOT EXISTS qr_report_v1.Assets (
+    id BINARY(16) NOT NULL,
+    building VARCHAR(255) NOT NULL,
+    roomNumber VARCHAR(255) NOT NULL,
+    appliance VARCHAR(255) NOT NULL,
+    service BOOLEAN DEFAULT FALSE,
+    userID BINARY(16) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (userID) REFERENCES qr_report_v1.Users(id)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB;
